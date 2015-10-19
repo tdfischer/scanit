5779340467d63bc8a124d7538cb19762a2eecf24
Torrie Fischer <tdfischer@hackerbots.net>
2015-10-18 18:16:10 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scanit',
+      version='0.0.1',
+      description='Scanit Scans It',
+      author='Torrie Fischer',
+      author_email='tdfischer@hackerbots.net',
+      modules=['scanit'],
+      scripts=['scanit']
+)
