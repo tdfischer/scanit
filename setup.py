8956b0a5110875fbd9fc01ef52cc57aba4320913
Torrie Fischer <tdfischer@hackerbots.net>
2015-10-18 18:18:53 -0700

    Move the setup.py version into scanit itself

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import scanit
+
 from distutils.core import setup
 
 setup(name='scanit',
-      version='0.0.1',
+      version=scanit.SCANIT_VERSION,
       description='Scanit Scans It',
       author='Torrie Fischer',
       author_email='tdfischer@hackerbots.net',
-      modules=['scanit'],
       scripts=['scanit']
 )
